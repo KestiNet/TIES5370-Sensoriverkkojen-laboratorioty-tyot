3f844c7949b2287a356325d7d858147b8a937f4f
KestiNet <esakesti@gmail.com>
2022-09-21 19:59:07 +0100

    Update Ajan_arviointi_peli.ino

--- Ajan_arviointi_peli/Ajan_arviointi_peli.ino
+++ Ajan_arviointi_peli/Ajan_arviointi_peli.ino
@@ -1,21 +1,14 @@
-int pushButton = 2;
-int count = 0;
-int val;
+int buttonPin = 2;
+int buttonValue;
 
-void setup()
-{
+
+void setup(){
+  pinMode(buttonPin, INPUT);
   Serial.begin(9600);
-  pinMode(pushButton, INPUT);
 }
 
-void loop()
-{
- count = digitalRead(pushButton);{
-   Serial.println(count);
-    count++;
- }
- 
-
- delay(10);
- 
-  }
+void loop(){
+  buttonValue = digitalRead(buttonPin);
+  Serial.println(buttonValue);
+  delay(500);
+}
