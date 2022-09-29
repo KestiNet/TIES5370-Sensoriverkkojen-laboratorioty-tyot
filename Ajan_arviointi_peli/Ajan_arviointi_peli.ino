b6170d5ccb3565a595c621259e2ff4461650cae0
KestiNet <esakesti@gmail.com>
2022-09-29 19:38:38 +0100

    Arduino button push count
    prints button pushes until 5

--- Ajan_arviointi_peli/Ajan_arviointi_peli.ino
+++ Ajan_arviointi_peli/Ajan_arviointi_peli.ino
@@ -1,14 +1,34 @@
-int buttonPin = 2;
-int buttonValue;
+const int buttonPin = 2;     
+unsigned long time1;
+unsigned long time2;
+int buttonState = 0;         
+unsigned long interval;
+int painallus = 0;
+//bool x = 0; //control
 
-
-void setup(){
-  pinMode(buttonPin, INPUT);
+void setup() {
+  pinMode(buttonPin, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
-void loop(){
-  buttonValue = digitalRead(buttonPin);
-  Serial.println(buttonValue);
-  delay(500);
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  
+  if (buttonState == LOW && painallus <= 4) {   //if button IS pushed
+   // x = 1;
+    time1 = millis();
+    while(buttonState == LOW) buttonState = digitalRead(buttonPin);
+    buttonState = HIGH;
+    painallus++;
+    Serial.println(painallus);
+  } 
+ // if (buttonState == LOW && x == 1) {   //if button IS pushed
+   // x = 0;
+    //time2 = millis();
+    //while(buttonState == LOW) buttonState = digitalRead(buttonPin);
+    //buttonState = HIGH;
+    //interval = time2 - time1;
+    //Serial.print("Interval:");
+    //Serial.println(interval);
+  
 }
