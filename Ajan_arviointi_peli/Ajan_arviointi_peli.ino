7df5efbb6676bf9569818a2f4e8957989b8f7b10
KestiNet <esakesti@gmail.com>
2022-10-11 20:01:36 +0100

    Update Ajan_arviointi_peli.ino
    lisaa harjoitusta

--- Ajan_arviointi_peli/Ajan_arviointi_peli.ino
+++ Ajan_arviointi_peli/Ajan_arviointi_peli.ino
@@ -1,20 +1,24 @@
-int buttonp = 0;
+int napinPainallus = 0;
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(2, INPUT);
-  digitalWrite(2, LOW);
-  attachInterrupt(2, button1, FALLING);
+Serial.begin(9600);
+pinMode(2, INPUT);
+digitalWrite(2, HIGH);
+attachInterrupt(2, painallus, FALLING);
+  
 }
+
 
 void loop(){
   
 }
 
-void button1(){
-  
-  while(buttonp <=4){
-    buttonp++;
-    Serial.println(buttonp);
-  }
+void painallus(){
+Serial.println(napinPainallus);
+if(napinPainallus <= 4){  
+  napinPainallus++;
+if (napinPainallus == 5){
+  Serial.println("lopeta");
+}
+}
 }
