73bd205bcdbee4458b6123ecf083bf2882f65991
KestiNet <esakesti@gmail.com>
2022-10-27 18:39:56 +0100

    interrupts and calculator works now

--- Ajan_arviointi_peli/Ajan_arviointi_peli.ino
+++ Ajan_arviointi_peli/Ajan_arviointi_peli.ino
@@ -1,34 +1,29 @@
-int napinPainallus = 0;
-long aloitus;
-long lopetus;
-byte ajastin;
-long kesto;
+const int buttonPin = 2;
+volatile bool buttonPushed = false;
+int laskuri = 0;
 
-void setup(){
-Serial.begin(9600);
-pinMode(2, INPUT);
-digitalWrite(2, HIGH);
-attachInterrupt(2, painallus, FALLING);
-  
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonISR, FALLING);
 }
 
 
-void loop(){
-  
+void loop() 
+{
+  if (buttonPushed && laskuri <=4)
+  {
+    delay(10);  // debounce time
+    buttonPushed = false;
+    laskuri++;
+    // TBD: Button push is detected.  Do the rest of your stuff here
+
+   Serial.println(laskuri);
+  }
 }
 
-void painallus(){
-Serial.println(napinPainallus);
-aloitus = millis();
-ajastin = 1;
-if(napinPainallus <= 4){  
-  napinPainallus++;
-if (napinPainallus == 5){
-  lopetus=millis();
-  ajastin = 0;
-  kesto = lopetus - aloitus;
-  Serial.println("lopeta");
-  Serial.println(kesto);
-}
-}
+void buttonISR()
+{
+  buttonPushed = true;
 }
