f8540cc4320044e0d7a515dc83decfe805765304
KestiNet <esakesti@gmail.com>
2023-01-24 00:14:35 +0000

    smart dimmer program started

--- Älykäs himmennin/Älykäs himmennin.ino
+++ Älykäs himmennin/Älykäs himmennin.ino
@@ -8,34 +8,23 @@ volatile bool h = false;
 int b1 = 3;
 const unsigned long intervalli = 50;
 unsigned long aiempiAika = 0;
+const int nopeaPainallus = 500;
 
-//määritellään keskeytykset ja pinmode ledille
+unsigned long painettuAika = 0;
+unsigned long vapautettuAika = 0;
+
 void setup(){
-    attachInterrupt(digitalPinToInterrupt(b1), kirkastaa, RISING);
+    attachInterrupt(digitalPinToInterrupt(b1), painallus, RISING);
     pinMode(led, OUTPUT);
     
   
 }
-// Tehdään looppiin määritykset joiden mukaan aletaan joko kirkastamaan tai himmentämään lediä
-// otetaan alusta millis() ja määritetään se nykyinenAika 
-//Jos nykyinenAika on suurempi tai yhtäsuuri kuin intervalli ja b1 nappia on painettu, aletaan kirkastamaan 
-//Jos nykyinenAika on suurempi tai yhtäsuuri kuin intervalli ja b2 nappia on painettu, aletaan himmentämään 
+unsigned long vapautettuAika = 0;
+//https://arduinogetstarted.com/tutorials/arduino-button-long-press-short-press
+//more instructions here
 
 void loop(){
-  unsigned long nykyinenAika = millis();
-  if((nykyinenAika - aiempiAika >= intervalli) &&  (k == true)){
-    analogWrite(led, kirkkaus);
-    delay(100);
-    kirkkaus = kirkkaus + muutos;
-    
-  aiempiAika = nykyinenAika;
-  }
-  if ((nykyinenAika - aiempiAika >= intervalli) && (h == true)){
-    analogWrite(led, kirkkaus);
-    delay(100);
-    kirkkaus = kirkkaus - muutos;
-    aiempiAika = nykyinenAika;   
-  }
+
   
 }
 
