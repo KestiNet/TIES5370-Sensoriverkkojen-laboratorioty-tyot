370d4aa9203a63c2b8ff8b558504c9cf54759da4
KestiNet <esakesti@gmail.com>
2023-01-30 00:11:36 +0000

    short press lights up the led to 50%

--- AlykasHimmennin/AlykasHimmennin.ino
+++ AlykasHimmennin/AlykasHimmennin.ino
@@ -1,54 +1,76 @@
-// määäritellään kaikki pinnit ja painikkeet
+/*
+* Muutetaan edellisessä tehtävässä toteutettua himmennintä siten, että nyt käytössä on vain yksi painike(B1). 
+Kun systeemi käynnistetään, niin led on sammuksissa ja napin nopea painallus sytyttää ledin 50% kirkkauteen maksimistaan. 
+Tämän jälkeen, kun painikkeen painaa alas, niin led lähtee kirkastumaan aina 5% 50 millisekunnin välein niin kauan, kun painike on pohjassa.
+Kun painike vapautetaan ja painetaanuudestaan alas, niin led himmenee vastaavasti, eli joka toinen pitkä painallus himmentää ja joka toinen kirkastaa. 
+Kun ledi on jossain muussa tilassa kuin sammuksissa niin napin nopea painallus sammuttaa ledinja vastaavasti kun led on sammuksissa niin napin nopea painallus sytyttää ledin siihen 50%:n kirkkauteen.Kommentoi koodisi
+*/
 
+// constants won't change. They're used here to set pin numbers:
+const int b1 = 3; // the number of the pushbutton pin
+const int katko = 500; // 500 milliseconds
 int led = 10;
-int kirkkaus = 0;
-int muutos = 5;
-volatile bool k = false;
-volatile bool h = false;
-int b1 = 3;
-const unsigned long intervalli = 50;
-unsigned long aiempiAika = 0;
-const int nopeaPainallus = 500;
 
-unsigned long painettuAika = 0;
+// Variables will change:
+int aiempiPainallus = LOW;  // the previous state from the input pin
+int nykyinenPainallus;     // the current reading from the input pin
+unsigned long painettuAika  = 0;
 unsigned long vapautettuAika = 0;
-int nykyinen;
-int viimeinenTila = LOW;
 
-void setup(){
-    attachInterrupt(digitalPinToInterrupt(b1), painallus, INPUT_PULLUP);
-    pinMode(led, OUTPUT);
-    
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(b1), painallus, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
   
 }
+/*
+keskeytysfunktio napin painallukselle
+Mittaa aikaa napin painalluksesta ja jos on nopea painallus, ledi syttyy 50% kirkkauteen
 
-//https://arduinogetstarted.com/tutorials/arduino-button-long-press-short-press
-//more instructions here
+*/
+void painallus(){
+  nykyinenPainallus = digitalRead(b1);
 
-void loop(){
-
-   //nykyinen = digitalRead(BUTTON_PIN);
-
-  if((k == true)&&(viimeinenTila == HIGH && nykyinen == LOW))        // button is pressed
+  if(aiempiPainallus == HIGH && nykyinenPainallus == LOW)        // button is pressed
     painettuAika = millis();
-  else if(viimeinenTila == LOW && nykyinen == HIGH) { // button is released
+  else if(aiempiPainallus == LOW && nykyinenPainallus == HIGH) { // button is released
     vapautettuAika = millis();
 
-    long painoAika = vapautettuAika - painettuAika;
+    long kesto = vapautettuAika - painettuAika;
 
-    if( painoAika < nopeaPainallus ){
+    if( kesto < katko )
       analogWrite(led, 127);
+      Serial.println("painettu");
+  }
+
+  aiempiPainallus = nykyinenPainallus;
+
+
+
+}
+
+  currentState = digitalRead(BUTTON_PIN);
+
+  if(lastState == HIGH && currentState == LOW) {        // button is pressed
+    pressedTime = millis();
+    isPressing = true;
+    isLongDetected = false;
+  } else if(lastState == LOW && currentState == HIGH) { // button is released
+    isPressing = false;
+  }
+
+  if(isPressing == true && isLongDetected == false) {
+    long pressDuration = millis() - pressedTime;
+
+    if( pressDuration > LONG_PRESS_TIME ) {
+      Serial.println("A long press is detected");
+      isLongDetected = true;
     }
-}
-}
-//keskeytys funktio jota kutsutaan kun nappia b1 painetaan
-void painallus(){
-  k = true;
+  }
 
-}
-//keskeytys funktio jota kutsutaan kun nappia b2 painetaan
-
-void himmentaa(){
-  h = true;
-
-}
+  // save the the last state
+  lastState = currentState;
