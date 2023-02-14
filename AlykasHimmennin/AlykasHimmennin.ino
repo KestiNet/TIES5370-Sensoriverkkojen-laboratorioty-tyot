ca96ac8764a1df4b6e4c59908ac67202b987b249
KestiNet <esakesti@gmail.com>
2023-02-14 00:06:56 +0000

    new library implemented

--- AlykasHimmennin/AlykasHimmennin.ino
+++ AlykasHimmennin/AlykasHimmennin.ino
@@ -1,70 +1,71 @@
-/*
-Tämän jälkeen, kun painikkeen painaa alas, niin led lähtee kirkastumaan aina 5% 50 millisekunnin välein niin kauan, kun painike on pohjassa.
-Kun painike vapautetaan ja painetaanuudestaan alas, niin led himmenee vastaavasti, eli joka toinen pitkä painallus himmentää ja joka toinen kirkastaa. 
-Kun ledi on jossain muussa tilassa kuin sammuksissa niin napin nopea painallus sammuttaa ledinja vastaavasti kun led on sammuksissa niin napin nopea painallus sytyttää ledin siihen 50%:n kirkkauteen.Kommentoi koodisi
-*/
+#include <Toggle.h>
 
 const int b1 = 3;
 const int led = 10;
-const int PITKA_PAINALLUS = 1000;
-const int muutos = 5;
-int kirkkaus = 0;
-int tavoiteKirkkaus = 255;
+byte kirkkaus;
 
-int viimeinenTila = LOW;
-int nykyinenTila;
+int sammuttaja;
 
-unsigned long nykyinenPainallus = 0;
-unsigned long vapautettuPainallus;
-long painonKesto;
+bool himmentaa;
 
-bool painaa = false;
-bool pitkaPainallusHavaittu = false;
-bool kierrattaja = false;
-int vaihtaja = 0;
-int luku = 0;
+Toggle nappi = (b1);
 
-void setup() {
- Serial.begin(9600);
+void setup(){
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
   pinMode(b1, INPUT);
-}
-
-void loop() {
-  
-  nykyinenTila = digitalRead(b1);
-  
-  if (viimeinenTila == HIGH && nykyinenTila == LOW){
-    nykyinenPainallus = millis();
-    painaa = true;
-    pitkaPainallusHavaittu = false;
-
-  }else if (viimeinenTila == LOW && nykyinenTila == HIGH){
-    painaa = false;
-
   }
 
-  if (painaa == true && pitkaPainallusHavaittu == false){
-    painonKesto = millis() - nykyinenPainallus;
 
-    if (painonKesto > PITKA_PAINALLUS){
-      pitkaPainallusHavaittu = true;
-                 
-      kirkastajaTaiHimmentaja();
-      
-      vaihtaja++;  
-      
-    }  
+void loop(){
 
-        
-
+  nappi.poll(); //alustaa napin käyttöön
+  if (nappi.onPress()) himmentaa = !himmentaa; // Jos nappia on painettu tarkistaa himmennetäänkö vai ei
+  if (nappi.isPressed()) {
+    (himmentaa) ? kirkkaus++ : kirkkaus--; //Riippuen painokerrasta aletaan kirkastamaan tai himmentämään
+    if (kirkkaus == 255) kirkkaus = 0; // määritellään mitä tehdään tiettyinä kirkkauksina
+    else if (kirkkaus >= 253) kirkkaus = 253;
+    delay(50); //tehdään 50 millisekunnin kesto
   }
-viimeinenTila = nykyinenTila;
+  analogWrite(led, kirkkaus);
+
+
 }
 
-/*aliohjelma joka tarkistaa onko nappi pohjassa ja onko pitkaPainallusHavaittu tosi
-  for silmukka määrittää kirkkauden muutoksen ja kasvattaa kirkkautta 5% 50millisekunnin välein
-*/
+
+
+/*
+nykyinenTila = digitalRead(b1);
+
+if (nykyinenTila != viimeinenTila) {
+    debounce = millis();
+    viimeinenTila = nykyinenTila;
+  }
+
+  if ((millis() - debounce) > DEBOUNCE_HIDASTUS) {
+    if (viimeinTasainenTila == HIGH && nykyinenTila == LOW){
+      nykyinenPainallus = millis();
+      painaa = true;
+      pitkaPainallusHavaittu = false;
+      Serial.println("painettu");
+    }else if (viimeinTasainenTila == LOW && nykyinenTila == HIGH){
+      painaa = false;
+      Serial.println("irroitettu");
+    }
+
+    if (painaa == true && pitkaPainallusHavaittu == false){
+      painonKesto = millis() - nykyinenPainallus;
+      if (painonKesto > PITKA_PAINALLUS){
+        pitkaPainallusHavaittu = true;
+
+        kirkastajaTaiHimmentaja();
+        vaihtaja++;
+        Serial.println(vaihtaja);
+      }
+    }
+    viimeinTasainenTila = nykyinenTila;
+  }
+}
 void kirkastajaTaiHimmentaja(){
 
   if ((painonKesto > PITKA_PAINALLUS) && (pitkaPainallusHavaittu == true) && (vaihtaja % 2 == 0)){
@@ -78,11 +79,7 @@ void kirkastajaTaiHimmentaja(){
       }
 
     }  
-    /*
-    samaan aliohjelmaan logiikka jolla voidaan havaita joka toinen napin painallus ja himmentää lediä sitä mukaan
-    himmentäminen ei toimi vielä, tarvitsee lisää työtä
-    TODO: mieti miten vähennetään kirkkautta 
-    */
+  
   if((painonKesto > PITKA_PAINALLUS) && (pitkaPainallusHavaittu == true) && (vaihtaja % 2 == 1)){
     kirkkaus = kirkkaus - muutos;
       for (kirkkaus = 255; kirkkaus > tavoiteKirkkaus; kirkkaus--){
@@ -93,3 +90,6 @@ void kirkastajaTaiHimmentaja(){
   }
   }
 }
+
+
+*/
