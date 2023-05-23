7df60879e031274f3c6227e3f1abf495363750d2
KestiNet <esakesti@gmail.com>
2023-05-23 18:36:09 +0100

    Tehtävä 3 valmis

--- Tehtava3/Tehtava3.ino
+++ Tehtava3/Tehtava3.ino
@@ -1,4 +1,6 @@
-int b1 = 3;
+#include <Arduino.h>
+
+const int b1 = 3;
 int led = 10;
 int tila = 0;
 int ledinTila = 0;
@@ -6,47 +8,57 @@ int maksimi = 255;
 int puolikas = 127;
 int viive = 20;
 int muutos = 8;
-signed int kirkasta = 0;
+volatile signed int kirkasta = 0;
 int painike = 2;
 int keskeytysViive = 100;
 
 void setup() {
+  Serial.begin(9600);
+  pinMode(b1, INPUT);
   attachInterrupt(digitalPinToInterrupt(b1), kirkastus, FALLING);
   pinMode(led, OUTPUT);
-  pinMode(b1, INPUT);
 }
 
 void kirkastus() {
+  Serial.println("Keskeytys");
   tila = 1;
   int tuloAika = millis();
 
   if (kirkasta == 0) {
     kirkasta = 1;
+    Serial.println("Kirkasta on 1");
   } else {
     kirkasta = 0;
+    Serial.println("Kirkasta on 0");
   }
-  delay(nykyinenAika - tuloAika > 100); 
 }
 
 void loop() {
-  if (tila) {
-    if (digitalRead(b1) == LOW && ledinTila == 0) {
+  if (tila == 1) {
+    delay(100);
+    int napinTila = digitalRead(b1);
+    if (napinTila == LOW && kirkasta == 0) {
+      Serial.println("eka iffi");
       ledinTila = puolikas;
-      analogWrite(led, ledinTila);
+      analogWrite(led, 255);
     }
 
-    if (digitalRead(b1) == LOW && ledinTila > 0) {
+    if (napinTila == LOW && kirkasta == 1) {
+      Serial.println("toka iffi");
       ledinTila = 0;
-      analogWrite(led, ledinTila);
+      analogWrite(led, 127);
     }
 
-    while (digitalRead(b1) == HIGH && ledinTila < 255 && kirkasta == 1) {
+    while (digitalRead(b1) == LOW && ledinTila < 255 && kirkasta == 1) {
+      Serial.println("eka while");
       ledinTila += muutos;
       analogWrite(led, ledinTila);
       delay(viive);
     }
+    
 
-    while (digitalRead(b1) == HIGH && ledinTila > 7 && kirkasta == 0) {
+    while (digitalRead(b1) == LOW && ledinTila > 7 && kirkasta == 0) {
+      Serial.println("toka while");
       ledinTila -= muutos;
       analogWrite(led, ledinTila);
       delay(viive);
